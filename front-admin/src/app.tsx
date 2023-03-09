6917413c7dd632ffca10a8013b782d647053173f
Katerina Slobodchikova <k.drazhina@gmail.com>
2023-03-09 12:30:25 +0300

    initial commit

---
+++ front-admin/src/app.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+// import AppRoutes from "app.routes";
+
+import "./app.css";
+import ErrorBoundaryComp from "./components/error-boundary.comp";
+import store from "./store";
+
+function App() {
+  return (
+    <ErrorBoundaryComp>
+      <Provider store={store}>
+        <Router>
+          {/* <AppRoutes /> */}
+          <div>Front-Admin</div>
+        </Router>
+      </Provider>
+    </ErrorBoundaryComp>
+  );
+}
+
+export default App;
