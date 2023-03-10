2efade604e3815ba7761aa5acb2ea0cc2ae19a38
Katerina <94918082+KaterinaSlobodchikova@users.noreply.github.com>
2023-03-10 10:52:04 +0300

    2 migrations added (#2)

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,17 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 import { DataSourceOptions } from "typeorm";
+import { $npmConfigName1678357923542 } from "../../migrations/1678357923542-$npm_config_name";
+import { $npmConfigName1678362416266 } from "../../migrations/1678362416266-$npm_config_name";
 
 const databaseConfig: DataSourceOptions = {
-    type: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: +process.env.POSTGRES_PORT,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    entities: [],
-    migrations: [],
-    migrationsTableName: "migrations_history",
-    migrationsRun: false,
-}
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [],
+  migrations: [$npmConfigName1678357923542, $npmConfigName1678362416266],
+  migrationsTableName: "migrations_history",
+  migrationsRun: false,
+};
 
 export default databaseConfig;
