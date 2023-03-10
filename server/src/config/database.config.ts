636fde2390de7666eb459ab67b22f0e415d394ec
Artyom Malyshev <58001922+Spectrages@users.noreply.github.com>
2023-03-10 11:30:22 +0300

    Added migrations (#6)

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import { DataSourceOptions } from "typeorm";
 import { $npmConfigName1678357923542 } from "../../migrations/1678357923542-$npm_config_name";
 import { $npmConfigName1678362416266 } from "../../migrations/1678362416266-$npm_config_name";
+import { $npmConfigName1678370951605 } from "../../migrations/1678370951605-$npm_config_name";
+import { $npmConfigName1678367706850 } from "../../migrations/1678367706850-$npm_config_name";
 
 const databaseConfig: DataSourceOptions = {
   type: "postgres",
@@ -11,7 +13,12 @@ const databaseConfig: DataSourceOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [],
-  migrations: [$npmConfigName1678357923542, $npmConfigName1678362416266],
+  migrations: [
+    $npmConfigName1678357923542, 
+    $npmConfigName1678362416266,
+    $npmConfigName1678367706850, 
+    $npmConfigName1678370951605,
+  ],
   migrationsTableName: "migrations_history",
   migrationsRun: false,
 };
