173e75cb7557d199886818e9397c0a266cb9142f
Katerina Slobodchikova <k.drazhina@gmail.com>
2023-03-14 10:55:37 +0300

    header component

---
+++ front-client/src/app/users/index.tsx
@@ -0,0 +1,16 @@
+// ======== react ============
+import React, { FC } from "react";
+
+// ======== components ============
+import PageHeaderComp from "../../components/page-header.comp";
+
+const UsersPage: FC = () => {
+  return (
+    <>
+      <PageHeaderComp />
+      
+    </>
+  );
+};
+
+export default UsersPage;
