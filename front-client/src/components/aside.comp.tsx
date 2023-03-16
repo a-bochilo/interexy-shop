5276b8190971ec597470c6b0b96aacf908f92c4f
Katerina Slobodchikova <k.drazhina@gmail.com>
2023-03-16 11:09:33 +0300

    aside fixed

--- front-client/src/components/aside.comp.tsx
+++ front-client/src/components/aside.comp.tsx
@@ -6,19 +6,18 @@ import Box from "@mui/material/Box";
 
 const PageAsideComp: FC = () => {
   return (
-    <aside>
-      <Box
-        sx={{
-          width: 300,
-          height: 700,
-          backgroundColor: "primary.dark",
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: [0.9, 0.8, 0.7],
-          },
-        }}
-      />
-    </aside>
+    <Box
+      component={"aside"}
+      sx={{
+        width: 300,
+        height: 700,
+        backgroundColor: "primary.dark",
+        "&:hover": {
+          backgroundColor: "primary.main",
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    />
   );
 };
 
