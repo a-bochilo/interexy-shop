da0a32323784b2848db25aff3f514ee6b147afdf
Katerina Slobodchikova <k.drazhina@gmail.com>
2023-03-16 14:42:57 +0300

    changes to drawer

--- front-client/src/components/navbar.comp.tsx
+++ front-client/src/components/navbar.comp.tsx
@@ -1,5 +1,5 @@
 // ========================== react ==========================
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 
 // ========================== mui ==========================
 import { styled, useTheme } from "@mui/material/styles";
@@ -14,8 +14,43 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import {
+  Container,
+  CssBaseline,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+
+const settings = ["Account", "Login"];
 
 const drawerWidth = 200;
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -29,7 +64,9 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 const PageNavBarComp: FC = () => {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
-  const [width, setWidth] = useState("8%");
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -39,34 +76,105 @@ const PageNavBarComp: FC = () => {
     setOpen(false);
   };
 
-  useEffect(() => {
-    open ? setWidth("20%") : setWidth("8%");
-  }, [open]);
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <Box
-      component={"nav"}
-      sx={{
-        minHeight: "0.25rem",
-      }}
-    >
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        edge="start"
-        sx={{
-          ...(open && { display: "none" }),
-          padding: 2,
-          minWidth: width,
-        }}
-      >
-        <MenuIcon onClick={handleDrawerOpen} />
-      </IconButton>
+    <Box component={"nav"} sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
 
+          <Container
+            maxWidth="xl"
+            sx={{
+              display: "flex",
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 3,
+              }}
+            >
+              <SearchIcon sx={{ cursor: "pointer" }} />
+              <ShoppingCartOutlinedIcon sx={{ cursor: "pointer" }} />
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar
+                    alt="Elvis Presley"
+                    src="/static/images/avatar/2.jpg"
+                  />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Container>
+        </Toolbar>
+      </AppBar>
       <Drawer
         sx={{
-          ...(open && { width: drawerWidth }),
+          width: drawerWidth,
           flexShrink: 0,
           "& .MuiDrawer-paper": {
             width: drawerWidth,
@@ -88,17 +196,7 @@ const PageNavBarComp: FC = () => {
         </DrawerHeader>
         <Divider />
         <List>
-          {["All Products", "Select Category"].map((text) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["Material", "Color", "Size"].map((text) => (
+          {["Catalog", "Cart", "Login"].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
                 <ListItemText primary={text} />
