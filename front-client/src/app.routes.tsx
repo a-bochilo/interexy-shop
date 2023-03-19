25a4433aa35a67b4b6d847f6c05475dfe5d88dca
Katerina Slobodchikova <k.drazhina@gmail.com>
2023-03-19 20:00:30 +0300

    CircularProgress added

--- front-client/src/app.routes.tsx
+++ front-client/src/app.routes.tsx
@@ -2,10 +2,20 @@
 import React, { FC, Suspense } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
 
+// ========================== mui ==========================
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
 // ======= private route ======= //
 const PrivateRoute: FC<{ element: any }> = ({ element: Element }) => {
   return true ? (
-    <Suspense fallback={<h2>🌀 Loading...</h2>}>
+    <Suspense
+      fallback={
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      }
+    >
       <div>
         <Element />
       </div>
@@ -17,7 +27,13 @@ const PrivateRoute: FC<{ element: any }> = ({ element: Element }) => {
 
 // ======= public route ======= //
 const PublicRoute: FC<{ element: any }> = ({ element: Element }) => (
-  <Suspense fallback={<h2>🌀 Loading...</h2>}>
+  <Suspense
+    fallback={
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    }
+  >
     <Element />
   </Suspense>
 );
