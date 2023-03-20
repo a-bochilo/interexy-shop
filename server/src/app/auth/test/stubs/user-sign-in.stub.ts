d67b6352791f3dcb78b2a72f1be2337354e6a02e
a-bochilo <103776395+a-bochilo@users.noreply.github.com>
2023-03-20 10:08:35 +0300

    Tests cart, products, security modules (#29)
    * cart tests implemented
    
    * product controller test implemented
    
    * product service test implemented
    
    * security service test implemented

--- server/src/app/auth/test/stubs/user-sign-in.stub.ts
+++
@@ -1,5 +0,0 @@
-import { UserSignInDto } from "../../dtos/user-sign-in.dto";
-
-export const UserSignInStub = (): UserSignInDto => {
-    return {email: "testmock@test.com", password: "123123123"}
-}
--- server/src/app/security/security.service.ts
+++ server/src/app/security/security.service.ts
@@ -16,35 +16,17 @@ export class SecurityService {
         private readonly userRepository: UserRepository,
         private readonly roleRepository: RoleRepository,
         private readonly jwtService: JwtService
-    ) { }
+    ) {}
 
     async generateJwt(user: UserEntity): Promise<TokenDto> {
-            const payload = await UserSessionDto.fromEntity(user);
-            const token = this.jwtService.sign(payload);
-            return { token };
-    }
-
-    async verifyJwt(jwt: string) {
-        try {
-            return await this.jwtService.verify(jwt);
-        } catch(error) {
-            throw new HttpException(
-                `${error}`,
-                HttpStatus.BAD_REQUEST
-            )
-        }
+        const payload = await UserSessionDto.fromEntity(user);
+        const token = this.jwtService.sign(payload);
+        return { token };
     }
 
     async getUser(id: string): Promise<UserEntity> {
-        try {
-            const user =  await this.userRepository.getById(id)
-            user.role = await this.roleRepository.getById(user.roleId);
-            return user;
-        } catch (error) {
-            throw new HttpException(
-                `${error}`,
-                HttpStatus.BAD_REQUEST
-            )
-        }
+        const user = await this.userRepository.getById(id);
+        user.role = await this.roleRepository.getById(user.roleId);
+        return user;
     }
 }
