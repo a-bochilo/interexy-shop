6e7628cb9f36d3619ca28459a53018154efd009b
Alexander Bochilo <a.bochilo@mail.ru>
2023-03-21 18:07:19 +0300

    basic functionality implemented

---
+++ front-client/src/api/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const $api = axios.create({
+    headers: {
+        "Content-type": "application/json",
+    },
+    baseURL: BASE_URL,
+});
+
+// $api.interceptors.response.use((response) => {
+//     if (response.status === 200) {
+//         console.log("Api: fetched successful");
+//     }
+//     return response;
+// });
+
+export default $api;
