4cfc8aab0d2e67719f3954e2b5f51f5e14e9b056
Alexander Bochilo <a.bochilo@mail.ru>
2023-03-27 18:59:33 +0300

    api 401 interceptor implemented

--- front-client/src/api/api.ts
+++ front-client/src/api/api.ts
@@ -2,38 +2,54 @@ import axios from "axios";
 import { decodeToken } from "react-jwt";
 
 const $api = axios.create({
-  headers: {
-    "Access-Control-Allow-Origin": "http://localhost:3001",
-    "Content-type": "application/json",
-  },
+    headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3001",
+        "Content-type": "application/json",
+    },
 });
 
 interface expireIn {
-  exp: number | null;
+    exp: number | null;
 }
 
 $api.interceptors.request.use(
-  async (config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    async (config) => {
+        config.headers.Authorization = `Bearer ${localStorage.getItem(
+            "token"
+        )}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
 
 $api.interceptors.response.use(async (response) => {
-  const token = window.localStorage.getItem("token");
-  if (token) {
-    const newUser: expireIn | null = decodeToken(token);
-    if (newUser !== null && newUser.exp !== null) {
-      const res = (newUser.exp - Date.now() / 1000) / 60;
-      if (res <= 30) {
-        localStorage.setItem(await $api.get("/auth/refresh-token"), "token");
-      }
+    const token = window.localStorage.getItem("token");
+    if (token) {
+        const newUser: expireIn | null = await decodeToken(token);
+        if (newUser !== null && newUser.exp !== null) {
+            const res = (newUser.exp - Date.now() / 1000) / 60;
+
+            if (res <= 30) {
+                localStorage.setItem(
+                    "token",
+                    await $api.get("/auth/refresh-token")
+                );
+            }
+        }
     }
-  }
-  return response;
+    return response;
 });
+
+$api.interceptors.response.use(
+    async (response) => response,
+    (error) => {
+        if (error.response.status === 401) {
+            window.location.replace("/auth/signIn");
+        }
+        return error;
+    }
+);
 
 export default $api;
